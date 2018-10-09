1ba3dc21bcc0125926bea6dc1708efab67063ac3
reuvec <chen.reuven@dell.com>
2018-10-09 17:19:19 +0300

    enable full screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,5 @@
 // App Configuration
-var elem = document.documentElement;
-
-/* View in fullscreen */
-function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) { /* Firefox */
-    elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE/Edge */
-    elem.msRequestFullscreen();
-  }
-}
-
-openFullscreen();
+document.documentElement.webkitRequestFullScreen();
 
 // App Init
 const video = document.getElementById('video');
